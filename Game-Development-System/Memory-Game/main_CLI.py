a02f08cefa28c2988966259c9249609c7741c706
Haykuhi Mkrtchyan <113100671+HaykuhiMk@users.noreply.github.com>
2024-12-02 15:38:51 +0400

    Add files via upload

---
+++ Game-Development-System/Memory-Game/main_CLI.py
@@ -0,0 +1,8 @@
+from memory_game import MemoryGame
+
+def main():
+    game = MemoryGame()
+    game.run()
+
+if __name__ == "__main__":
+    main()
---
+++ Game-Development-System/Memory-Game/main_GUI.py
@@ -0,0 +1,5 @@
+import memory_game_GUI 
+
+root = memory_game_GUI.tk.Tk()
+game = memory_game_GUI.MemoryGameGUI(root)
+root.mainloop()
